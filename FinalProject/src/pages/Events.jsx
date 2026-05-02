a981030132a8debb6eed6fd1a2d04b6769387dc9
realhesh <hesham.ahmed.1426@gmail.com>
2026-05-02 20:16:34 +0300

    add Events.jsx

--- FinalProject/src/pages/Events.jsx
+++ FinalProject/src/pages/Events.jsx
@@ -3,21 +3,22 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import EventCard from '../components/EventCard';
 import { useState } from 'react';
 import {initialEvents} from "../Event";
-
+import { Routes, Route } from 'react-router-dom';
+import EventDetails from './EventDetails';
 function Events() {
   return (
-    <div>
+    <>
+    <div style={{justifyContent: 'center', display: 'flex', flexWrap: 'wrap', gap: '20px', padding: '20px' }}>
       <h1>Events</h1>
-      <p>List of upcoming events will be displayed here.</p>
-      <input type="text" placeholder="Search events" />
-      <DropdownButton id="dropdown-basic-button" title="All">
-      <Dropdown.Item href="#/action-1">Tech</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Career</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Sports</Dropdown.Item>
-      <Dropdown.Item href="#/action-4">Arts</Dropdown.Item>
-    </DropdownButton>
-
     </div>
+    <div>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}>
+            {initialEvents.map((event) => (
+                <EventCard key={event.id} event={event} />
+            ))}
+        </div>
+    </div>
+    </>
   );
 }
 
