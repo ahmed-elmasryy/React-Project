2acf40c7f9d900300b633d6e6f20339132c0faeb
realhesh <hesham.ahmed.1426@gmail.com>
2026-05-02 18:23:08 +0300

    changed color of text in navbar

--- FinalProject/src/components/Navbar.jsx
+++ FinalProject/src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom'; 
-
+import './Navbar.css';
 function NavbarComp() {
   return (
-    <Navbar bg="primary" data-bs-theme="light">
+    <Navbar bg="primary">
       <Container>
         <Nav className="me-auto">
-        <Nav.Link as={Link} to="/Home">Home</Nav.Link>
-        <Nav.Link as={Link} to="/Events">Events</Nav.Link>
-        <Nav.Link as={Link} to="/CreateEvent">Create Event</Nav.Link>
-        <Nav.Link as={Link} to="/MyEvents">My Events</Nav.Link>
+        <Nav.Link as={Link} to="/Home" style={{ color: 'white' }}>Home</Nav.Link>
+        <Nav.Link as={Link} to="/Events" style={{ color: 'white' }}>Events</Nav.Link>
+        <Nav.Link as={Link} to="/CreateEvent" style={{ color: 'white' }}>Create Event</Nav.Link>
+        <Nav.Link as={Link} to="/MyEvents" style={{ color: 'white' }}>My Events</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
